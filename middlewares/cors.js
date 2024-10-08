42245ddc4f5bf7f59b34bedeec7eec0578c49141
.Vibhuu <144808765+VVUx21@users.noreply.github.com>
2024-10-08 13:20:12 +0530

    Updated cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,8 +1,20 @@
 import cors from "cors";
+const allowedOrigins = [
+  'http://localhost:3000',
+];
 
-const corsOptions = {
-    origin: "*",
-    optionsSuccessStatus: 200,
+const corsOptions ={
+  origin: function (origin, callback) {
+    // Allow requests with no origin like mobile apps or curl requests
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+  credentials: true // Allow credentials (cookies, authorization headers, etc.)
 };
 
 const corsConfig = cors(corsOptions);
